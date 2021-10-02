f8ba26fb3f65d9e7dee42bd9cd8bf772011768c2
Michail <misha220993@gmail.com>
2021-10-02 13:41:59 +0400

    Lesson_4

--- 4l_MikhailKirzhner.playground/Contents.swift
+++ 4l_MikhailKirzhner.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import Foundation
+import Darwin
 
 enum engineState: String{
     case on = "Двигатель запущен"
@@ -11,35 +12,25 @@ enum windowsState: String{
     case close = "Окна закрыты"
 }
 
-enum trunk {
-    case empty(trunkEmpty: String)
-    case full(trunkfull: String)
+enum trunkStatic {
+    case empty
+    case half
+    case full
     case kg(volumeKG: Double)
 }
-
-enum spoilerStaticSportCar: String{
-    case raised = "Спойлер поднят"
-    case hidden = "Спойлер спрятон"
-}
-
-enum NOSStationSportCar{
-    case on
-    case off
-}
-
-
 
 
 class Car{
     let mark: String
     let release: Int
-    let volumeTrunk: trunk
+    let volumeTrunk: trunkStatic
     var actionEngine: engineState
     var window: windowsState
-    var staticTrunk: trunk
+    var staticTrunk: trunkStatic
+    static var carCount = 0
 
     
-    init(mark: String, release: Int, vTrunk: trunk, actionEngine: engineState,window: windowsState, trunk: trunk){
+    init(mark: String, release: Int, vTrunk: trunkStatic, actionEngine: engineState,window: windowsState, trunk: trunkStatic){
         self.mark = mark
         self.release = release
         self.window = window
@@ -60,18 +51,31 @@ class Car{
         print("Авто марки \(mark), \(release) года выпуска. Двигатель \(actionEngine.rawValue). Объем багажника \(volumeTrunk)")
     }
 }
-var car2 = Car(mark: "Ford", release: 2020, vTrunk: .kg(volumeKG: 380), actionEngine: .on, window: .close, trunk: .empty(trunkEmpty: "Багажник пуст"))
-print(car2.actionEngine.rawValue)
+
 class SportCar: Car{
+    
+    enum spoilerStaticSportCar: String{
+        case raised = "Спойлер поднятый"
+        case hidden = "Спойлер спрятон"
+    }
+
+    enum NOSStationSportCar{
+        case on
+        case off
+    }
     
     var spoiler: spoilerStaticSportCar
     var NOS: NOSStationSportCar = .off
     
-    init(mark: String, release: Int, vTrunk: trunk, actionEngine: engineState, window: windowsState, trunk: trunk, spoiler: spoilerStaticSportCar) {
+        init(mark: String, release: Int, vTrunk: trunkStatic, actionEngine: engineState, window: windowsState, trunk: trunkStatic, spoiler: spoilerStaticSportCar) {
         self.spoiler = spoiler
+        SportCar.carCount += 1
         super.init(mark: mark, release: release, vTrunk: vTrunk, actionEngine: actionEngine, window: window, trunk: trunk)
     }
-    
+    deinit{
+        SportCar.carCount -= 1
+        print("Таких авто больше нет")
+    }
     //Функция выключает двигатель и закрывает окна
     
     override func activeEngine(action: engineState){
@@ -79,7 +83,7 @@ class SportCar: Car{
         case .off:
             self.actionEngine = .off
             self.window = .close
-            print("\(actionEngine.rawValue),\(self.window)")
+            print("\(actionEngine.rawValue),\(self.window.rawValue)")
         case .on:
             self.actionEngine = .on
             print("\(actionEngine.rawValue)")
@@ -89,6 +93,7 @@ class SportCar: Car{
     //Функция поднимает спойлер и опускает спойлер. Если поднят окра закрываются, иначе окна открыты
     
     func spoilerAction(action: spoilerStaticSportCar){
+
         switch action {
         case .hidden:
             self.spoiler = .raised
@@ -114,15 +119,62 @@ class SportCar: Car{
             }
         }
     }
+    
+    override func discriptionCar(){
+        print("Авто марки \(mark), \(release) года выпуска. Двигатель \(actionEngine.rawValue). Объем багажника \(volumeTrunk). \(spoiler.rawValue).")
+    }
 }
 class TrunkCar: Car{
     
+    enum LightsStatic: String{
+        case on = "Фары включены"
+        case off = "Фары выключены"
+    }
+    
+    var Lights: LightsStatic = .off
+    
+    //Функция определяет текущее время и решает включить фары или нет
+    func activeFogLights(active: LightsStatic){
+        
+        let Time = (Calendar.current.component(.hour, from: Date()))
+        
+        switch active{
+        case .on:
+            if(Time >= 17){
+                self.Lights = .on
+                print("Время \(Time). Фары включены")
+            }else{
+                print("Время \(Time). Светло, фары не нужны")
+            }
+        case .off:
+            self.Lights = .off
+            print("Фары выключены")
+        }
+    }
+    
+    override func discriptionCar(){
+        print("Авто марки \(mark), \(release) года выпуска. Двигатель \(actionEngine.rawValue). Объем багажника \(volumeTrunk). \(Lights.rawValue).")
+    }
 }
 
 
+var TrunkCarVolvo = TrunkCar(mark: "Volvo", release: 2009, vTrunk: .kg(volumeKG: 12000), actionEngine: .off, window: .close, trunk: .empty)
 
-var car1 = SportCar(mark: "Ford", release: 2020, vTrunk: .kg(volumeKG: 380), actionEngine: .on, window: .close, trunk: .empty(trunkEmpty: "Багажник пуст"), spoiler: .hidden)
-car1.spoilerAction(action: .raised)
-car1.NOSAction(action: .on)
+TrunkCarVolvo.activeEngine(action: .off)
+print(TrunkCarVolvo.actionEngine.rawValue)
+print(TrunkCarVolvo.release)
+TrunkCarVolvo.activeFogLights(active: .on)
+SportCarFord.discriptionCar()
+
+
+
+var SportCarFord = SportCar(mark: "Ford", release: 2020, vTrunk: .kg(volumeKG: 380), actionEngine: .on, window: .close, trunk: .empty, spoiler: .hidden)
+
+SportCarFord.activeEngine(action: .off)
+print(SportCarFord.actionEngine.rawValue)
+print(SportCarFord.release)
+SportCarFord.spoilerAction(action: .raised)
+SportCarFord.discriptionCar()
+SportCarFord.NOSAction(action: .on)
 
 
